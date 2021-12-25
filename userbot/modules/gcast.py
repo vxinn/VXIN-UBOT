ad28ad70dfe11a9ed3558165786381b67e8a3c96
vxinn <96599804+vxinn@users.noreply.github.com>
2021-12-25 21:30:37 +0700

    Update gcast.py

--- userbot/modules/gcast.py
+++ userbot/modules/gcast.py
@@ -39,7 +39,7 @@ async def gcast(event):
     else:
         await event.edit("**Berikan Sebuah Pesan atau Reply**")
         return
-    kk = await event.edit("`𝙼𝙴𝙽𝙶𝙸𝚁𝙸𝙼 𝚅𝙸𝚁𝚄𝚂 𝙺𝙴 𝙶𝙻𝙾𝙱𝙰𝙻𝙻𝚈📡🦠...`")
+    kk = await event.edit("`𝙼𝙴𝙽𝙶𝙸𝚁𝙸𝙼 𝚅𝙸𝚁𝚄𝚂 𝙺𝙴 𝙶𝙻𝙾𝙱𝙰𝙻𝙻𝚈🦠`")
     er = 0
     done = 0
     async for x in event.client.iter_dialogs():
